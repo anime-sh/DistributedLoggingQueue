fe9120ed73762e511e2e579bcd77cbd261196588
Aryan Singh <ary1733@gmail.com>
2023-01-31 22:59:44 +0530

    Tested reg con and pro PartA

--- test_asgn1/py_test_PartA.py
+++ test_asgn1/py_test_PartA.py
@@ -15,10 +15,7 @@ try:
     r = requests.post(url, json=data)
     r.raise_for_status()
     response = r.json()
-    if response["status"] == "Success":
-        print("Sent successfully")
-    else:
-        print("Failed")
+
     print("Response")
     print(response)
 except requests.exceptions.HTTPError as errh:
@@ -28,7 +25,29 @@ except requests.exceptions.ConnectionError as errc:
     print("Error Connecting:", errc)
 
 print()
-print("Testing the endpoint get Topic")
+print("Testing the endpoint create Topic (topic already exists case)")
+url = base_url + "/topics"
+data = {
+    "topic_name": "tp_1"
+}
+
+try:
+    print(f"request = {data}")
+    r = requests.post(url, json=data)
+    r.raise_for_status()
+    response = r.json()
+
+    print("Response")
+    print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+
+print()
+print("Testing the endpoint get Topics")
 url = base_url + "/topics"
 
 
@@ -43,3 +62,107 @@ except requests.exceptions.HTTPError as errh:
 
 except requests.exceptions.ConnectionError as errc:
     print("Error Connecting:", errc)
+
+
+print()
+print("Testing the endpoint register consumer")
+url = base_url + "/consumer/register"
+data = {
+    "topic": "tp_1"
+}
+
+consumer_id = None
+try:
+    print(f"request = {data}")
+    r = requests.post(url, json=data)
+    r.raise_for_status()
+    response = r.json()
+    print("Response")
+    consumer_id = response['consumer_id']
+    print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+
+print()
+print("Testing the endpoint register consumer (case with invalid topic name)")
+url = base_url + "/consumer/register"
+data = {
+    "topic": "tp_2"
+}
+
+try:
+    print(f"request = {data}")
+    r = requests.post(url, json=data)
+    r.raise_for_status()
+    response = r.json()
+    print("Response")
+    print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+
+print()
+print("Testing the endpoint register producer")
+url = base_url + "/producer/register"
+data = {
+    "topic": "tp_1"
+}
+producer_id = None
+try:
+    print(f"request = {data}")
+    r = requests.post(url, json=data)
+    r.raise_for_status()
+    response = r.json()
+    print("Response")
+    producer_id = response["producer_id"]
+    print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+print()
+print("Testing the endpoint register producer (missing topic name)")
+url = base_url + "/producer/register"
+data = {
+    "topic": "tp_2"
+}
+try:
+    print(f"request = {data}")
+    r = requests.post(url, json=data)
+    r.raise_for_status()
+    response = r.json()
+    print("Response")
+    print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+print()
+print("calling the endpoint get Topics")
+url = base_url + "/topics"
+
+
+try:
+    r = requests.get(url)
+    r.raise_for_status()
+    response = r.json()
+    print("Response")
+    print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+print("Note : Topic tp_2 was created while registering the producer.")
