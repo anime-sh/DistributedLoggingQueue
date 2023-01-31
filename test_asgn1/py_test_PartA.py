56e5a64a05856b46a0f14d007111953afeb768e5
Aryan Singh <ary1733@gmail.com>
2023-01-31 23:21:15 +0530

    Completed partA py_test

--- test_asgn1/py_test_PartA.py
+++ test_asgn1/py_test_PartA.py
@@ -3,6 +3,7 @@ HOST = "127.0.0.1"
 PORT = 5000
 base_url = f"http://{HOST}:{PORT}"
 
+num_of_messages = 3
 
 print("Testing the endpoint create Topic")
 url = base_url + "/topics"
@@ -166,3 +167,69 @@ except requests.exceptions.ConnectionError as errc:
     print("Error Connecting:", errc)
 
 print("Note : Topic tp_2 was created while registering the producer.")
+
+
+print()
+print(f"Pushing {num_of_messages} messages on Topic tp_1")
+
+
+url = base_url + "/producer/produce"
+
+
+try:
+    for i in range(1,num_of_messages+1):
+        data = {
+            "topic": "tp_1",
+            "producer_id": producer_id,
+            "message": f"LOG MESSAGE {i}"
+        }
+        print(f"request = {data}")
+        r = requests.post(url, json=data)
+        r.raise_for_status()
+        response = r.json()
+        print("Response")
+        print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+
+
+print()
+
+print(f"Consuming messages using the consumer {consumer_id} and reporting size for {num_of_messages+1} times")
+
+
+
+
+try:
+    data = {
+        "topic": "tp_1",
+        "consumer_id": consumer_id,
+    }
+    print(f"request = {data}")
+    for i in range(num_of_messages+1):
+        print()
+
+        url = url = base_url + "/size"
+        print(f"Size of tp_1 for given consumer")
+        r = requests.get(url, json=data)
+        response = r.json()
+        print("Response")
+        print(response)
+        
+        print()
+        print(f"Consuming messages")
+        url = base_url + "/consumer/consume"
+        
+        r = requests.get(url, json=data)
+        r.raise_for_status()
+        response = r.json()
+        print("Response")
+        print(response)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error:", errh)
+
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
